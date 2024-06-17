990c6cf2657ad0bb35fb9f3c61493efb894626bf
reedanj <james.r@unitaskr.com>
2024-06-17 13:45:49 +0100

    add tests for leaderboard endpoint

--- __tests__/alphabet.test.js
+++ __tests__/alphabet.test.js
@@ -14,14 +14,34 @@ describe("GET /api/alphabet", () => {
       .expect(200)
       .expect("Content-Type", "application/json; charset=utf-8")
       .then((res) => {
+        expect(res.body.characters).toBeInstanceOf(Array);
         expect(res.body.characters.length).toBe(24);
+        res.body.characters.forEach((character) => {
+          expect(character).toMatchObject({
+            alphabet_id: expect.any(Number),
+            greek_letter: expect.any(String),
+            phonetic_equivalent: expect.any(String),
+            pronounced_as: expect.any(String),
+          });
+        });
       });
   });
 });
 
 describe("GET /api/alphabet/:id", () => {
   it("responds with object with a character from the greek alphabet", () => {
-    return request(app).get("/api/alphabet/1").expect(200).expect("Content-Type", "application/json; charset=utf-8");
+    return request(app)
+      .get("/api/alphabet/1")
+      .expect(200)
+      .expect("Content-Type", "application/json; charset=utf-8")
+      .then((res) => {
+        expect(res.body.character).toMatchObject({
+          alphabet_id: 1,
+          greek_letter: "Α α - Alpha",
+          phonetic_equivalent: "a",
+          pronounced_as: "a in father",
+        });
+      });
   });
 });
 
