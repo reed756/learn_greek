8d12595413281d16035030b3f116c811b1811c11
reedanj <james.r@unitaskr.com>
2024-11-13 19:02:28 +0000

    add env variables

--- db/index.js
+++ db/index.js
@@ -1,5 +1,18 @@
 import pg from "pg";
+import { dirname } from "node:path";
+import { fileURLToPath } from "node:url";
+import dotenv from "dotenv";
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
 const { Pool } = pg;
+const ENV = process.env.NODE_ENV || "development";
+
+const pathToCorrectEnvFile = `${__dirname}/../.env.${ENV}`;
+
+dotenv.config({
+  path: pathToCorrectEnvFile,
+});
 
 if (!process.env.PGDATABASE) {
   throw new Error("No PGDATABASE configured");
--- db/seeds/seed.js
+++ db/seeds/seed.js
@@ -1,7 +1,7 @@
 import { insertData } from "./insert-data.js";
 import { dropTables, createTables } from "./manage-tables.js";
 
-const seed = async () => {
+const seed = async ({ itemData }) => {
   return dropTables()
     .then(() => {
       return createTables();
