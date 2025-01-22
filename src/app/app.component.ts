b8916ac30b0e5047db0bb8c7d86ef26d009eb89f
reedanj <james.r@unitaskr.com>
2025-01-22 17:50:45 +0000

    add routing and pages

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core'
-import { RouterOutlet } from '@angular/router'
 import { NavBarComponent } from './shared/nav-bar/nav-bar.component'
+import { RouterOutlet } from '@angular/router'
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, NavBarComponent],
+  imports: [NavBarComponent, RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
---
+++ src/app/modules/alphabet/alphabet.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-alphabet',
+  imports: [],
+  templateUrl: './alphabet.component.html',
+  styleUrl: './alphabet.component.scss'
+})
+export class AlphabetComponent {
+
+}
