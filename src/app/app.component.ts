ca45ed8d61a8386c6397f05413c1521481b24c29
reedanj <james.r@unitaskr.com>
2025-01-22 14:19:57 +0000

    add prettier rules

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,11 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component } from '@angular/core'
+import { RouterOutlet } from '@angular/router'
+import { NavBarComponent } from './shared/nav-bar/nav-bar.component'
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, NavBarComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
-export class AppComponent {
-  title = 'learn-greek';
-}
+export class AppComponent {}
