bc187263aa6f7c5f7c3e57b7ae46f21ba08e4326
reedanj <james.r@unitaskr.com>
2025-08-24 11:55:11 +0100

    add image directive

--- src/app/modules/home/card/card.component.spec.ts
+++ src/app/modules/home/card/card.component.spec.ts
@@ -1,7 +1,8 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { CardComponent } from './card.component';
+import { NgOptimizedImage } from '@angular/common';
 import { provideRouter } from '@angular/router';
+import { CardComponent } from './card.component';
 
 describe('CardComponent', () => {
   let component: CardComponent;
@@ -9,13 +10,21 @@ describe('CardComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [CardComponent],
+      imports: [CardComponent, NgOptimizedImage],
       providers: [provideRouter([])],
     }).compileComponents();
 
     fixture = TestBed.createComponent(CardComponent);
     component = fixture.componentInstance;
-    fixture.detectChanges();
+    const testConfig = {
+      title: 'Test Title',
+      description: 'Test Description',
+      imageUrl: 'assets/test-image.png',
+      linkText: '/test-path',
+    };
+    fixture.componentRef.setInput('cardConfig', testConfig);
+    // wait for initial data binding
+    await fixture.whenStable();
   });
 
   it('should create', () => {
--- src/app/modules/home/card/card.component.ts
+++ src/app/modules/home/card/card.component.ts
@@ -1,10 +1,11 @@
+import { NgOptimizedImage } from '@angular/common';
 import { Component, input } from '@angular/core';
 import { RouterLink } from '@angular/router';
 import { CardConfig } from '../../../shared/interfaces/card';
 
 @Component({
   selector: 'app-card',
-  imports: [RouterLink],
+  imports: [RouterLink, NgOptimizedImage],
   templateUrl: './card.component.html',
   styleUrl: './card.component.scss',
 })
